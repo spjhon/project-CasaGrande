e088c1bbbdfae1a2e1a02165cbb99a0f4d1a7e91
Juan Camilo Aistizabal <spjhon@gmail.com>
2025-07-19 14:37:20 -0500

    Se implement i18n de forma mas facil que el año pasado

---
+++ fronend_casagrande/src/app/not-found.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import cavernicola from "../../public/dribbble_1.gif";
+import Image from "next/image";
+import styles from "./notFound.module.css";
+
+// Render the default Next.js 404 page when a route
+// is requested that doesn't match the middleware and
+// therefore doesn't have a locale associated with it.
+
+export default function NotFound() {
+  return (
+   
+      
+     <>
+        <div className={styles.container}>
+          <Image className={styles.picture} src={cavernicola} alt="Cavernicola" />
+        </div>
+        <h1 className={styles.notFound}>Page Not Found / Página No Encontrada</h1>
+      
+    </>
+  );
+}
